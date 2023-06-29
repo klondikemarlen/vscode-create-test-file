17fef702f63a62aff05a40a8bf3d91ff6405ceda
Marlen Brunner <klondikemarlen@gmail.com>
2023-06-29 15:10:05 -0700

    :white_check_mark: Add minimal end-to-end test.
    Testing should be much easier now.

--- src/test/suite/createTestFile.test.ts
+++ src/test/suite/createTestFile.test.ts
@@ -1,6 +1,38 @@
 import * as assert from 'assert';
+import * as vscode from 'vscode';
 
-import { testPath } from '../../createTestFile';
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { createTestFile, testPath } from '../../createTestFile';
+
+const WORKSPACE_ROOT = path.resolve(__dirname, '../example-workspace');
+
+suite('createTestFile', () => {
+	setup(() => {
+		const testFileExamplePath = path.join(WORKSPACE_ROOT, 'test_example.js');
+		return fs.unlink(
+			testFileExamplePath,
+			error => {
+				if (error) {
+				  throw error;
+				}
+
+				console.log(`${testFileExamplePath} is deleted.`);
+			  }
+		);
+	});
+
+	test('should create the appropriate a test file', () => {
+		const examplePath = path.join(WORKSPACE_ROOT, './example.js');
+		const originalUri = vscode.Uri.file(examplePath);
+
+		return createTestFile(originalUri).then(newUri => {
+			const expected = path.join(WORKSPACE_ROOT, './test_example.js');
+			assert.equal(expected, newUri.path);
+		});
+	});
+});
 
 
 suite('testPath', () => {
