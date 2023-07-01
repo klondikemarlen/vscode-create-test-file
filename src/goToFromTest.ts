e693f3ae41136c795fedacbb629a49aec22ce449
Marlen Brunner <klondikemarlen@gmail.com>
2023-07-01 12:17:37 -0700

    :sparkles: Most basic swap to or from test file.
    I need a way to enhance the isTestFile check, and a way to clear the cache.
    Also I should reflect on whether I'm using the cache correctly or not.

--- src/goToFromTest.ts
+++ src/goToFromTest.ts
@@ -1,10 +1,51 @@
 import * as vscode from 'vscode';
 
-export function goToOrFromTest(context: vscode.ExtensionContext, srcUri: vscode.Uri): Thenable<vscode.Uri> {
+import { createTestFile } from './createTestFile';
 
-    return Promise.resolve(srcUri);
+export function goToOrFromTest(extensionContext: vscode.ExtensionContext, sourceUri: vscode.Uri): Thenable<vscode.Uri> {
+
+    if (isTestFile(extensionContext, sourceUri)) {
+        const destinationUri = goToSource(extensionContext, sourceUri);
+        return Promise.resolve(destinationUri);
+    } else {
+        return goToTest(extensionContext, sourceUri);
+    }
 }
 
-// Cache lookup
-//
-// context.workspaceState:
+export function isTestFile(extensionContext: vscode.ExtensionContext, sourceUri: vscode.Uri): boolean {
+    const workspaceState = extensionContext.workspaceState;
+
+    // if entry found that links this file to a source file
+    // then the file is a test file
+    if (workspaceState.get(`test-to-source-map:${sourceUri.path}`)) {
+        return true;
+    }
+
+    return false;
+}
+
+export function goToSource(extensionContext: vscode.ExtensionContext, sourceUri: vscode.Uri): vscode.Uri {
+    const workspaceState = extensionContext.workspaceState;
+    const destinationPath = workspaceState.get(`test-to-source-map:${sourceUri.path}`) as string;
+    if (destinationPath) {
+        const destinationUri = vscode.Uri.file(destinationPath);
+        return destinationUri;
+    }
+    vscode.window.showErrorMessage('Could not find a source file for this test file.');
+    return sourceUri;
+}
+
+export function goToTest(extensionContext: vscode.ExtensionContext, sourceUri: vscode.Uri): Thenable<vscode.Uri> {
+    const workspaceState = extensionContext.workspaceState;
+    const destinationPath = workspaceState.get(`source-to-test-map:${sourceUri.path}`) as string;
+    if (destinationPath) {
+        const destinationUri = vscode.Uri.file(destinationPath);
+        return Promise.resolve(destinationUri);
+    }
+
+    return createTestFile(sourceUri).then(destinationUri => {
+        workspaceState.update(`source-to-test-map:${sourceUri.path}`, destinationUri.path);
+        workspaceState.update(`test-to-source-map:${destinationUri.path}`, sourceUri.path);
+        return destinationUri;
+    });
+}
